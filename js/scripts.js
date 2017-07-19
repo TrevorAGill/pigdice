88929d346c0b1f4d4cb81d0b6e66ca2a751580d9
Kate Byars and Trevor Gill <kate+trevor@>
2017-07-19 15:45:52 -0700

    finalize totalScore and temporaryScore counts.

--- js/scripts.js
+++ js/scripts.js
@@ -28,29 +28,68 @@ $(document).ready(function(){
   }
 
 //Front End
-  $("#playerOneRoll").click(function(event) {
-    var diceRoll = (getRandomIntInclusive(1,6));
-    $(".diceNumber").text(diceRoll);
-    $(".tempScoreDisplay").text(Player1.addToTemp(diceRoll));
-  });
 
-  $("#playerTwoRoll").click(function(event) {
-    var diceRoll = (getRandomIntInclusive(1,6));
-    $(".diceNumber").text(diceRoll);
-    $(".tempScoreDisplay").text(Player2.addToTemp(diceRoll));
-  });
+    $("#playerOneRoll").click(function(event) {
+      $(".center").removeClass("middle");
+      var diceRoll = (getRandomIntInclusive(1,6));
+      $(".diceNumber").text(diceRoll);
+      if(diceRoll === 1) {
+        document.getElementById("playerOneHold").disabled = true;
+        document.getElementById("playerOneRoll").disabled = true;
+        document.getElementById("playerTwoRoll").disabled = false;
+        this.temporaryScore = 0
+        $(".tempScoreDisplay").text(0);
+        $(".center").addClass("middle");
+        Player2.temporaryScore = 0;
+      } else if (diceRoll > 1) {
+        $(".tempScoreDisplay").text(Player1.addToTemp(diceRoll));
+        return this.temporaryScore;
+      }
+    });
 
-  $("#playerOneHold").click(function(event) {
-    $(".playerOneTotal").text(Player1.addToTotal(this.totalScore));
-    document.getElementById("playerOneRoll").disabled = true;
-    document.getElementById("playerTwoRoll").disabled = false;
-    this.temporaryScore = 0;
-  });
+    $("#playerTwoRoll").click(function(event) {
+      $(".center").removeClass("middle");
+      var diceRoll = (getRandomIntInclusive(1,6));
+      $(".diceNumber").text(diceRoll);
+      if(diceRoll === 1) {
+        document.getElementById("playerTwoHold").disabled = true;
+        document.getElementById("playerTwoRoll").disabled = true;
+        document.getElementById("playerOneRoll").disabled = false;
+        this.temporaryScore = 0
+        $(".tempScoreDisplay").text(0);
+        $(".center").addClass("middle");
+        Player1.temporaryScore = 0;
+      } else if (diceRoll > 1) {
+      $(".tempScoreDisplay").text(Player2.addToTemp(diceRoll));
+      }
+    });
 
-  $("#playerTwoHold").click(function(event) {
-    $(".playerOneTotal").text(Player1.addToTotal(this.totalScore));
-    document.getElementById("playerTwoRoll").disabled = true;
-    document.getElementById("playerOneRoll").disabled = false;
-    this.temporaryScore = 0;
-  });
+    $("#playerOneHold").click(function(event) {
+      $(".playerOneTotal").text(Player1.addToTotal(this.totalScore));
+      document.getElementById("playerOneRoll").disabled = true;
+      document.getElementById("playerTwoRoll").disabled = false;
+      $(".totalScoreDisplayP1").hide();
+      $(".tempScoreDisplay").text("");
+      $(".diceNumber").text("");
+      Player2.temporaryScore = 0;
+        if (Player1.totalScore >= 100) {
+          alert("you win!")
+        }
+    });
+
+    $("#playerTwoHold").click(function(event) {
+      $(".playerTwoTotal").text(Player2.addToTotal(this.totalScore));
+      document.getElementById("playerTwoRoll").disabled = true;
+      document.getElementById("playerOneRoll").disabled = false;
+      $(".totalScoreDisplayP2").hide();
+      $(".tempScoreDisplay").text("");
+      $(".diceNumber").text("");
+      Player1.temporaryScore = 0;
+        if (Player2.totalScore >= 100) {
+        alert("you win!")
+      }
+    });
+  // } else if(Player1.totalScore >= 100 || Player2.totalScore >= 100) {
+  //   alert("you win");
+  // }
 });
