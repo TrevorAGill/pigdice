24aae0617e4bf579a85b487ff7e15fc099b544e6
Kate Byars and Trevor Gill <kate@>
2017-07-20 12:08:59 -0700

    add a really nice, animated die

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
     this.playerName = playerName;
   }
 
+  function Dice (diceName) {
+    this.diceName = diceName;
+  }
+
+  // Dice.prototype.addDiceRoll = function(){
+  //   $(".cube").removeClass("spin");
+  //   $(".cube").addClass("spin");
+  // }
+
   var player1 = new Player(0,0);
   var player2 = new Player(0,0);
 
@@ -29,6 +38,28 @@ $(document).ready(function(){
 
 //Front End
 
+
+//   $('.cube').click.animate(function() {
+//     // $('.cube').animate(function(){
+//     opacity: 0.25,
+//     left: "+=50",
+//     // height: "toggle"
+//     //
+//   // });
+// });
+
+//
+// $(window).click(function() {
+//   $( ".cube" ).animate({
+//     $('.cube').css('transform', 'rotateX(10deg)');
+//     // $('.cube').css('transform', 'rotateX(-40deg)' + 'rotateY(40deg)');
+//     // $('.cube').css('transform', 'rotateX(50deg)' + 'rotateY(-50deg)');
+//     // $('.cube').css('transform', 'rotateX(-10deg)' + 'rotateY(10deg)');
+//   }, 1500 );
+// });
+
+
+
     $("#NameSubmit").click(function(event) {
       player1.playerName = $("#PlayerOneName").val();
       player2.playerName = $("#PlayerTwoName").val();
@@ -38,6 +69,12 @@ $(document).ready(function(){
     });
 
     $("#playerOneRoll").click(function(event) {
+      $(".cube").removeClass("spin-vertical");
+
+      setTimeout(function() {
+      $(".cube").addClass("spin-vertical");
+    }, 0);
+
       document.getElementById("playerTwoHold").disabled = false;
       $(".center").removeClass("middle");
       var diceRoll = (getRandomIntInclusive(1,6));
@@ -51,12 +88,14 @@ $(document).ready(function(){
         $(".center").addClass("middle");
         Player2.temporaryScore = 0;
       } else if (diceRoll > 1) {
-        $(".tempScoreDisplay").text(Player1.addToTemp(diceRoll));
+        $(".tempScoreDisplay").text(player1.addToTemp(diceRoll));
         return this.temporaryScore;
       }
     });
 
     $("#playerTwoRoll").click(function(event) {
+      var diceTwo = new Dice(diceTwo);
+      diceTwo.addDiceRoll();// $(".cube").addClass("spin");
       document.getElementById("playerOneHold").disabled = false;
       $(".center").removeClass("middle");
       var diceRoll = (getRandomIntInclusive(1,6));
@@ -68,34 +107,36 @@ $(document).ready(function(){
         this.temporaryScore = 0
         $(".tempScoreDisplay").text(0);
         $(".center").addClass("middle");
-        Player1.temporaryScore = 0;
+        player1.temporaryScore = 0;
       } else if (diceRoll > 1) {
-      $(".tempScoreDisplay").text(Player2.addToTemp(diceRoll));
+      $(".tempScoreDisplay").text(player2.addToTemp(diceRoll));
       }
     });
 
     $("#playerOneHold").click(function(event) {
-      $(".playerOneTotal").text(Player1.addToTotal(this.totalScore));
+      // $(".cube").removeClass("spin");
+      $(".playerOneTotal").text(player1.addToTotal(this.totalScore));
       document.getElementById("playerOneRoll").disabled = true;
       document.getElementById("playerTwoRoll").disabled = false;
       $(".totalScoreDisplayP1").hide();
       $(".tempScoreDisplay").text("");
       $(".diceNumber").text("");
-      Player2.temporaryScore = 0;
+      player2.temporaryScore = 0;
         if (Player1.totalScore >= 100) {
           $(".win").show();
         }
     });
 
     $("#playerTwoHold").click(function(event) {
-      $(".playerTwoTotal").text(Player2.addToTotal(this.totalScore));
+      $(".cube").removeClass("spin");
+      $(".playerTwoTotal").text(player2.addToTotal(this.totalScore));
       document.getElementById("playerTwoRoll").disabled = true;
       document.getElementById("playerOneRoll").disabled = false;
       $(".totalScoreDisplayP2").hide();
       $(".tempScoreDisplay").text("");
       $(".diceNumber").text("");
-      Player1.temporaryScore = 0;
-        if (Player2.totalScore >= 100) {
+      player1.temporaryScore = 0;
+        if (player2.totalScore >= 100) {
           $(".win").show();
       }
     });
